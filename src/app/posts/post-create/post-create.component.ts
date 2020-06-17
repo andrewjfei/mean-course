28c9aab67d060b4bd94af91f2e1c81bd8102732f
andrewjfei <andrewjfei@gmail.com>
2020-06-17 21:01:14 +1200

    uploaded image is viewable from the client browser

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading: boolean = false;
   form: FormGroup;
+  imagePreview: string;
 
   constructor(
     private postsService: PostsService, 
@@ -25,7 +26,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
-      content: new FormControl(null, { validators: [Validators.required] })
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] })
     });
 
     this.route.paramMap.subscribe(
@@ -62,6 +64,17 @@ export class PostCreateComponent implements OnInit {
     }
 
     this.form.reset();
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
   }
 
 }
