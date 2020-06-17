b445b5866b1b7668477a7e6c6900fb7f2da4eff0
andrewjfei <andrewjfei@gmail.com>
2020-06-17 12:14:35 +1200

    form created using Angular forms TD approach

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  // posts = [
+  //   { title: 'First Post', content: 'Dummy content' },
+  //   { title: 'Second Post', content: 'Dummy content' },
+  //   { title: 'Third Post', content: 'Dummy content' }
+  // ];
+  
+  @Input() posts: Post[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
